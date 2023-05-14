8152c370233d23bd33ce7a6b30e65a2d04b8d25d
Aswin Sunil <Aswinsunil14@gmail.com>
2023-05-14 15:50:29 +0530

    Server

---
+++ client/src/components/common/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { ProfileProps } from 'interfaces/common'
+
+
+const Profile = ({type, name, avatar, email, properties}: ProfileProps) => {
+  return (
+    <div>ProfileComponent</div>
+  )
+}
+
+export default Profile
---
+++ client/src/pages/home.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {useList} from '@pankod/refine-core'
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from 'components'
+import { Box, Stack } from '@mui/system'
+import { Typography } from '@pankod/refine-mui'
+
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color='#11142D'>
+        Dashboard
+      </Typography>
+      <Box mt='20px' display='flex' flexWrap="wrap" gap={4}>
+        <PieChart 
+        title="Properties for sale"
+        value={684}
+        series={[75, 25]}
+        colors={['#275be8', '#c4e8ef']}
+        />
+         <PieChart 
+        title="Properties for Rent"
+        value={550}
+        series={[80, 20]}
+        colors={['#275be8', '#c4e8ef']}
+        />
+         <PieChart 
+        title="Total Customers"
+        value={6684}
+        series={[75, 25]}
+        colors={['#275be8', '#c4e8ef']}
+        />
+         <PieChart 
+        title="Properties for Cities"
+        value={680}
+        series={[30, 70]}
+        colors={['#275be8', '#c4e8ef']}
+        />
+      </Box>
+      <Stack mt="25px" width="100%"
+      direction={{ xs: 'column', lg: 'row' }}
+      gap={4}
+      >
+        <TotalRevenue />
+        <PropertyReferrals />
+
+      </Stack>
+    </Box>
+
+  )
+}
+
+export default Home
